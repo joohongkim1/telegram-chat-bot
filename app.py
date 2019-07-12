f8b37ca01a356a5af7fa5d643c808c6fa09959f1
joohongkim1 <wnghd9938@naver.com>
2019-07-12 16:41:47 +0900

    Implement Translation functionality using Naver Papago

--- app.py
+++ app.py
@@ -3,8 +3,10 @@ from decouple import config
 import pprint
 import requests
 app = Flask(__name__)
-API_TOKEN = config('API_TOKEN')  # 상수는 대문자
 
+API_TOKEN = config('API_TOKEN')  # 상수는 대문자
+NAVER_CLIENT_ID = config('NAVER_CLIENT_ID')
+NAVER_CLIENT_SECRET = config('NAVER_CLIENT_SECRET')
 # POST 요청 - 회원가입 등 정보를 숨겨야 할 때 
 
 
@@ -25,10 +27,38 @@ def telegram():
     if from_telegram.get('message') is not None:
         # 우리가 원하는 로직
         chat_id = from_telegram.get('message').get('chat').get('id')
-        text = from_telegram.get('message').get('text')
+        text = from_telegram.get('message').get('text')  # 사용자가 보낸 텍스트
 
-        if text == '점심메뉴':
-            text = '짜장면이나 먹어'
+        # 첫 네글자가 '/한영 ' 일 때 확인
+        if text[0:4] == '/한영 ':  # str값도 인덱스로 접근 가능, 각각의 캐릭터로 접근
+            headers = {  # 요청에 대한 정보가 저장
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,  # 끝에 , 넣어주면 좋음
+            }
+            data = {
+                'source': 'ko',
+                'target': 'en',
+                'text': text[4:] # '/번역 ' 이후의 문자열만 대상으로 번역
+            }
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            pprint.pprint(papago_res.json())
+            text = papago_res.json().get('message').get('result').get('translatedText')
+
+        elif text[0:4] == '/영한 ':  # str값도 인덱스로 접근 가능, 각각의 캐릭터로 접근
+            headers = {  # 요청에 대한 정보가 저장
+                'X-Naver-Client-Id': NAVER_CLIENT_ID,
+                'X-Naver-Client-Secret': NAVER_CLIENT_SECRET,  # 끝에 , 넣어주면 좋음
+            }
+            data = {
+                'source': 'en',
+                'target': 'ko',
+                'text': text[4:] # '/번역 ' 이후의 문자열만 대상으로 번역
+            }
+            papago_url = 'https://openapi.naver.com/v1/papago/n2mt'
+            papago_res = requests.post(papago_url, headers=headers, data=data)
+            pprint.pprint(papago_res.json())
+            text = papago_res.json().get('message').get('result').get('translatedText')
 
         # Send Message API URL 
         base_url = 'https://api.telegram.org'
@@ -36,6 +66,7 @@ def telegram():
         requests.get(api_url)
     
     return '', 200
+
 
 
 if __name__ == '__main__':
